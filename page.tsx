959a6ac4cdf6f927a6d54c99f5560d6805690cf1
Wai86788 <161097005+Wai86788@users.noreply.github.com>
2024-03-05 21:57:30 +0530

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,17 @@
+import Header from "./componets/Header"
+import Main from "./componets/Main"
+import Footer from "./componets/Footer"
+
+
+
+export default function Home() {
+  return (
+    <>
+     <Header/>
+     <Main/>
+     <Footer/>
+    
+    </>
+
+  )
+}
